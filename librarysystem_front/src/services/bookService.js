04fb4f543a2d4e66793cda4e97c3bc17e6ebf9c1
hcoh99 <hcoh990310@gmail.com>
2025-12-23 15:45:12 +0900

    백엔드 주소연결

--- librarysystem_front/src/services/bookService.js
+++ librarysystem_front/src/services/bookService.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 // 공통 axios 인스턴스
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+  //baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+
+  baseURL: "http://k8s-default-backendi-920c8fb194-1130524743.us-east-2.elb.amazonaws.com",
 });
 
 // 매 요청마다 토큰 자동 첨부 (로그인 후 localStorage에 accessToken 저장했다고 가정)
